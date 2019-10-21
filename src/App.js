cab20da8c331b3e84626e108590326cc7533f488
ahaigh9877 <alastairhaigh@me.com>
2019-10-21 10:39:55 +0200

    spinning head

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from './logo.svg';
+import logo from "./images/bwitton.png";
+import "./App.css";
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Fuck off, World.
+          Fuck off, World. No, really.
         </a>
       </header>
     </div>
